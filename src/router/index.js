e50c7a1589534a49dde18daf2100dd9d997b9373
wangbo <2373265157@qq.com>
2018-11-01 20:46:44 +0800

    提交

--- src/router/index.js
+++ src/router/index.js
@@ -1,9 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import index from '@/components/index'
-import axios from 'axios'
 
-Vue.use(Router,axios)
+Vue.use(Router)
 
 export default new Router({
   routes: [
