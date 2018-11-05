7b6fe275306ef09bd583482469ad39576c736a3f
rowanh <houchangbbgs@163.com>
2018-11-05 09:14:49 +0800

    设置

---
+++ src/components/sz.vue
@@ -0,0 +1,74 @@
+<template>
+  <div id="app">
+    <div class="box">
+      <div class="header">
+        <p>&lt;</p>
+        <p>意见反馈</p>
+        <p @click="aa">发送</p>
+      </div>
+      <div class="center">
+        <textarea style="overflow-y:scroll" name="" id="" cols="100%" rows="20" placeholder="请输入反馈,我们将为您不断改进">
+        </textarea>
+      </div>
+
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: "box",
+  methods:{
+    aa(){
+      alert('反馈成功！我们会更加努力');
+    }
+  }
+};
+</script>
+
+<style>
+.box {
+  display: flex;
+  height: 100%;
+  height: 1920px;
+  border: 2px solid orange;
+  display: -webkit-box;
+  -webkit-box-orient: vertical;
+  -webkit-box-direction: normal;
+}
+.header {
+  width: 100%;
+  height: 190px;
+  background: #494961;
+  display: flex;
+}
+.header p {
+  flex: 1;
+  color: #fff;
+  font-size: 50px;
+  text-align: center;
+  line-height: 190px;
+}
+.header p:first-child {
+  text-align: left;
+  padding-left: 30px;
+  font-size: 80px;
+}
+.header p:last-child {
+  font-size: 40px;
+  font-weight: normal;
+  cursor: pointer;
+}
+.center {
+  width: 600px;
+  height: 500px;
+  display: flex;
+  margin: 100px auto;
+  background: #ccc;
+}
+textarea {
+  background: #f4f4f4;
+  font-size: 30px;
+}
+</style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,16 +1,29 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import index from '@/components/index'
-import axios from 'axios'
+// import index from '@/components/index'
+// import sz from '@/components/sz'
+import sz2 from '@/components/sz2'
 
-Vue.use(Router,axios)
+
+
+Vue.use(Router)
 
 export default new Router({
   routes: [
-    {
+    /*  {
+       path: '/',
+       name: 'index',
+       component: index
+     }, */
+    // {
+    //   path: '/',
+    //   name: 'sz',
+    //   component: sz
+    // },
+     {
       path: '/',
-      name: 'index',
-      component: index
+      name: 'sz2',
+      component: sz2
     }
   ]
 })
